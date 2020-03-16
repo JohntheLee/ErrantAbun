d52bf8f3f54a0e643373c26439d28285dc7ffbc3
Jeonghoon Lee <jeonghoon.lee@mail.utoronto.ca>
2020-03-16 01:46:53 -0400

    Addition of package details.

---
+++ R/errantabun.R
@@ -0,0 +1,32 @@
+#' Errant Abundance Matrix Function
+#'
+#' This function allows one to create an errant abundance matrix using an original abundance matrix and a random statistical distribution of choice.
+#'
+#' @param abun_matrix The original abundance matrix. Columns must represent species, and rows must represent plots/blocks/etc.
+#' @param method The statistical distribution of choice: "Poisson" or "binomial".
+#' @param prob The probability weight used to construct the binomial distribution. Not relevant for Poisson distribution.
+#'
+#' @keywords errant, abundance, distribution, Poisson, binomial, random
+#'
+#' errantabun()
+
+
+
+errantabun <- function(abun_matrix, method, prob){
+  list_errant_abun <- list()
+  species_list <- as.numeric(factor(colnames(abun_matrix)))
+  for(i in seq_along(species_list)){
+    if(method == "Poisson"){
+      poisson_abun <- rpois(n = nrow(abun_matrix), lambda = abun_matrix[ , i])
+      list_errant_abun[[i]] <- poisson_abun
+    }
+    else if(method == "binomial"){
+      binom_abun <- rbinom(n = nrow(abun_matrix), size = abun_matrix[ , i], prob = prob)
+      list_errant_abun[[i]] <- binom_abun
+    }
+  }
+  return(data.frame((list_errant_abun)) %>%
+           rename_all(funs(quo(colnames(abun_matrix)))) %>%
+           cbind(rownames(abun_matrix), .) %>%
+           rename("Plot" = "rownames(abun_matrix)"))
+}
