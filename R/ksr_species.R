4d01be8e8f02628bb864fa16eb0f5c11a388c8b9
Jeonghoon Lee <jeonghoon.lee@mail.utoronto.ca>
2020-11-12 23:25:56 -0500

    Addition of ksr_species data set and its documentation

---
+++ R/ksr_species.R
@@ -0,0 +1,29 @@
+#' Species abundance matrix from KSR
+#'
+#' This species abundance matrix is collected from the Koffler Scientific Reserve at Joker's Hill (University of Toronto).
+#' Fourteen blocks serve as disparate communities of grassland plant species. Each block contains an array of different-sized plots: 8 0.5m^2, 8 1m^2, and 4 2m^2 plots.
+#'
+#' @docType data
+#'
+#' @usage data(ksr_species)
+#'
+#' @format A data frame with 280 rows and 74 variables:
+#' \describe{
+#' \item{Block}{block number}
+#' \item{Plot}{plot number}
+#' \item{Plot_size}{size of plot, in m^2}
+#' \item{species}{scientific name of species}
+#' }
+#'
+#' @keywords dataset, species, abundance
+#'
+#' @references Leila Forsyth. TBD
+#'
+#' @source Leila Forsyth. TBD
+#'
+#' LazyData: true
+#'
+#' @examples
+#' ksr_species
+
+"ksr_species"
