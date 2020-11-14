b71610489740ff6b60ee0747974e06eda9789750
Jeonghoon Lee <jeonghoon.lee@mail.utoronto.ca>
2020-11-14 15:44:48 -0500

    Update of name and description; addition of example usage

--- R/ksr_species.R
+++ R/ksr_species.R
@@ -1,13 +1,14 @@
-#' Species abundance matrix from KSR
+#' Species abundance dataset from KSR
 #'
-#' This species abundance matrix is collected from the Koffler Scientific Reserve at Joker's Hill (University of Toronto).
+#' This species abundance dataset is collected from the Koffler Scientific Reserve at Joker's Hill (University of Toronto).
 #' Fourteen blocks serve as disparate communities of grassland plant species. Each block contains an array of different-sized plots: 8 0.5m^2, 8 1m^2, and 4 2m^2 plots.
+#' This abundance dataset must be transformed into an abundance matrix, with columns representing species and rows representing the plot/block/etc. of choice.
 #'
 #' @docType data
 #'
 #' @usage data(ksr_species)
 #'
-#' @format A data frame with 280 rows and 74 variables:
+#' @format A dataset with 280 rows and 74 variables:
 #' \describe{
 #' \item{Block}{block number}
 #' \item{Plot}{plot number}
@@ -24,6 +25,6 @@
 #' LazyData: true
 #'
 #' @examples
-#' ksr_species
+#' ksr_species_matrix <- ksr_species %>% filter(Size == 2) %>% unite(Plot, c(Block, Plot), remove = TRUE, sep = ".") %>% transform(Plot = as.numeric(Plot)) %>% column_to_rownames(var = "Plot") %>% data.matrix
 
 "ksr_species"
