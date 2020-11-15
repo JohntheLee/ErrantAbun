7f412dabcd634512bc037e5335d592cb2a50d286
Jeonghoon Lee <jeonghoon.lee@mail.utoronto.ca>
2020-11-15 00:07:40 -0500

    Removed @export

--- R/ksr_species.R
+++ R/ksr_species.R
@@ -30,7 +30,5 @@
 #' @examples
 #' ksr_species_matrix <- ksr_species %>% filter(Size == 2) %>% unite(Plot, c(Block, Plot), remove = TRUE, sep = ".") %>% transform(Plot = as.numeric(Plot)) %>% column_to_rownames(var = "Plot") %>% data.matrix
 #'
-#' @export
-#' ksr_species
 
 "ksr_species"
