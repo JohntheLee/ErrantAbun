7298b4b749fd8dc5435ba4c167a4895c86deb9dd
Jeonghoon Lee <jeonghoon.lee@mail.utoronto.ca>
2020-11-16 16:54:21 -0500

    Description update; usage update; format update; references and sources update; example update

--- R/ksr_species.R
+++ R/ksr_species.R
@@ -3,32 +3,37 @@
 #' @description
 #' This species abundance dataset is collected from the Koffler Scientific Reserve at Joker's Hill (University of Toronto).
 #'
-#' Fourteen blocks serve as disparate communities of grassland plant species. Each block contains an array of different-sized plots: 8 0.5m^2, 8 1m^2, and 4 2m^2 plots.
+#' Fourteen blocks serve as disparate communities of grassland plant species. Each block contains an array of different-sized plots: eight 0.5m^2, eight 1m^2, and four 2m^2 plots.
 #'
 #' This abundance dataset must be transformed into an abundance matrix, with columns representing species and rows representing the plot/block/etc. of choice.
 #'
 #' @docType data
 #'
-#' @usage data(ksr_species)
+#' @usage View(ksr_species)
 #'
 #' @format A dataset with 280 rows and 74 variables:
 #' \describe{
-#'  \item{Block}{block number}
-#'  \item{Plot}{plot number}
+#'  \item{Block}{block number, ranges from 1-14}
+#'  \item{Plot}{plot number, ranges from 1-8}
 #'  \item{Plot_size}{size of plot, in m^2}
 #'  \item{species}{scientific name of species}
 #' }
 #'
 #' @keywords dataset, species, abundance
 #'
-#' @references Leila Forsyth. TBD
+#' @references Leila Forsyth. Department of Ecology and Evolutionary Biology (University of Toronto). TBD
 #'
-#' @source Leila Forsyth. TBD
+#' @source Leila Forsyth. Department of Ecology and Evolutionary Biology (University of Toronto). TBD
 #'
 #' LazyData: true
 #'
 #' @examples
-#' ksr_species_matrix <- ksr_species %>% filter(Size == 2) %>% unite(Plot, c(Block, Plot), remove = TRUE, sep = ".") %>% transform(Plot = as.numeric(Plot)) %>% column_to_rownames(var = "Plot") %>% data.matrix
+#' ksr_species_matrix <- ksr_species %>%
+#'  filter(Size == 2) %>%
+#'  unite(Plot, c(Block, Plot), remove = TRUE, sep = ".") %>%
+#'  transform(Plot = as.numeric(Plot)) %>%
+#'  column_to_rownames(var = "Plot") %>%
+#'  data.matrix
 #'
 
 "ksr_species"
