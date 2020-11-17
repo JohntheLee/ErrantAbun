61f2adb2aed4aa4f680d2c618bd8817347471e23
Jeonghoon Lee <jeonghoon.lee@mail.utoronto.ca>
2020-11-16 19:24:07 -0500

    Rework of example ksr abundance matrix

---
+++ R/ksr.R
@@ -0,0 +1,24 @@
+#' Species abundance matrix from Koffler Scientific Reserve
+#'
+#' @description
+#' This species abundance matrix is collected from the Koffler Scientific Reserve at Joker's Hill (University of Toronto).
+#'
+#' Fourteen blocks serve as disparate communities of grassland plant species. Each block contains an array of different-sized plots: eight 0.5m^2, eight 1m^2, and four 2m^2 plots.
+#'
+#' Each column of this abundance matrix represents a grassland plant species. Each row represents one of four 2m^2 plots from all 14 blocks.
+#'
+#' @docType data
+#'
+#' @usage ksr
+#'
+#' @format A dataset with 56 rows and 71 variables:
+#'
+#' @keywords dataset, species, abundance, matrix
+#'
+#' @references Leila Forsyth. Department of Ecology and Evolutionary Biology (University of Toronto). TBD
+#'
+#' @source Leila Forsyth. Department of Ecology and Evolutionary Biology (University of Toronto). TBD
+#'
+#' LazyData: true
+
+"ksr"
